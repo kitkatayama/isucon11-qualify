225fdef808ea0efae4822334ce9fa4835586b38b
kitkatayama <9929234+kitkatayama@users.noreply.github.com>
2021-08-21 03:55:38 +0000

    goをdisableしてたりサーバごとに変えたり

--- pre.sh
+++ pre.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -ex
 
+/home/isucon/env.sh
+
+#------------
+# APP server
+#------------
+
+if [ `uname -n` = "ip-192-168-0-11" ] || [ `uname -n` = "ip-192-168-0-12" ]; then 
+
 ## stop services
 
 #sudo systemctl stop mysql
 sudo systemctl stop nginx
-sudo systemctl disable --now isucondition.go.service
+sudo systemctl stop isucondition.go.service
 sleep 1
 
 ## language specific build code here
@@ -19,9 +27,17 @@ sudo truncate --size 0 /var/log/nginx/access.log
 
 # sudo systemctl start mysql
 sleep 1
-sudo systemctl start --now isucondition.go.service
+sudo systemctl start isucondition.go.service
 sudo systemctl start nginx
 
-## initialize code here
-~isucon/webapp/sql/init.sh
+fi
+
+#------------
+# DB server
+#------------
+
+if [ `uname -n` = "ip-192-168-0-13" ] ; then 
+
+/home/isucon/webapp/sql/init.sh
 sleep 1
+fi
