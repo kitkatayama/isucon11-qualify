4bf1ad7f7919da605527dcdad79b96add5921131
kitkatayama <9929234+kitkatayama@users.noreply.github.com>
2021-08-21 02:28:06 +0000

    add pre bench script

---
+++ pre.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+## stop services
+
+#sudo systemctl stop mysql
+sudo systemctl stop nginx
+sudo systemctl disable --now isucondition.go.service
+sleep 1
+
+## language specific build code here
+cd webapp/go && make
+
+## log rotate
+sudo cp /var/log/nginx/access.log /var/log/nginx/access.log-`date "+%s"`
+sudo truncate --size 0 /var/log/nginx/access.log
+
+## start service
+
+# sudo systemctl start mysql
+sleep 1
+sudo systemctl start --now isucondition.go.service
+sudo systemctl start nginx
+
+## initialize code here
+~isucon/webapp/sql/init.sh
+sleep 1
